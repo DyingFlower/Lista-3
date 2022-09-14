270f413c0f59223d961cf3fd17b489ea1ae86d0f
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-09-14 16:07:48 -0300

    Create questão 6.java

---
+++ questão 6.java
@@ -0,0 +1,35 @@
+package Lista3;
+import java.util.Scanner;
+public class questao6 {
+	public static void inversaodeordem(int vetor[]) {
+		/* Sua função deverá retornar um vetor de ordem inversa.
+	Desta forma, o primeiro elemento passa ser o último elemento do vetor, o
+	segundo elemento passa ser o penúltimo, assim, sucessivamente; */
+		int i;
+		int k=vetor.length;
+		int vetornovo[]= new int [vetor.length];
+		for(i=0;i<vetor.length;i++) {
+			k--;
+			vetornovo[k]=vetor[i];
+			
+		}
+		for(i=0;i<vetornovo.length; i++) {
+			System.out.print("["+ vetornovo[i] +"]");
+		}
+	}
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("Diga quantos elementos tem seu vetor");
+		int t = leitor.nextInt();
+		int vetor[]= new int [t];
+		System.out.println("Se for usar a função de compactação ou descompactação digite apenas 0 ou 1");
+		for(int i=0;i<t;i++) {
+			System.out.println("Escreva o elemento: " + i);
+			vetor[i]= leitor.nextInt();
+		}
+		inversaodeordem(vetor);
+		leitor.close();
+
+	}
+
+}
