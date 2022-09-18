ca24dfd41c57bc5df5cef7c125d51f084c939a9d
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2022-09-18 04:19:44 -0300

    Update questão 14.java

--- questão 14.java
+++ questão 14.java
@@ -1,50 +1,58 @@
 package Lista3;
 import java.util.Scanner;
-public class questao14 {
-	public static void Contagemdecaracteres(String vetor[]) {
-		/* Sua função deverá contar os caracteres que
-	aparecem no vetor. Os caracteres podem ser qualquer letra ou número do
-	alfabeto. */
-	    String vetornovo[]= new String[100];
-        int contador;
-        int vetorc[]=new int[100];
-        int i, j, n = 0;
-        int a=0;
-        int c=0;
-        int tam = vetor.length;
-        for( i = 0; i < tam; i++){
+public class questao14{
+	public static void Contagemdecaracteres(String vetor[]){
+		 String vetorigual[]= new String[vetor.length];
+	        int vetorc[]= new int[100];
+	        int a=0;
+	        int i,j;
+	        int contador;
+	        int n = vetor.length;
 
-            for( j = 0; j < n; j++ ){
-                if( vetor[i] == vetornovo[j] )
-                    break;
-            }
+	        for(i=0;i<vetor.length;i++){
+	            vetorigual[i]=vetor[i];
+	        }
+	        for (i=0;i<n;i++){
+	            int k=i+1;
+	            int removidos=0;
+	            for(j=i+1;j<n;j++){
+	                if(vetor[j].charAt(0)==vetor[i].charAt(0)){
+	                    removidos++;
+	                }
+	                else{
+	                    vetor[k++]=vetor[j];
 
-            if( j == n ){
-                vetornovo[n] = vetor[i];
-                n++;
-                c++;
-            }
-        }
-        for(i=0;i<c;i++){
-            contador=0;
-            for(j=0;j<vetor.length;j++){
-                if(vetornovo[i]==vetor[j]){
-                    contador++;
-                }
-            }
-            vetorc[a]=contador;
-            a+=1;
-        }
-        for( i = 0; i < c; i++ ){
-            System.out.print(vetornovo[i]+"="+vetorc[i]+" ");
-        }
-    }
-	
-	public static void main(String[] args) {
-		Scanner leitor = new Scanner(System.in);
-		String vetor[]={"a","b","c","3","3","5","a","c"};// quando usa o Scanner o codigo buga, não sei pq;
-		Contagemdecaracteres(vetor);
-		leitor.close();
+	                }
+	            }
+	            n=n-removidos;
+	        }
+	        for(i=0;i<n;i++){
+	            contador=0;
+	            for(j=0;j<vetorigual.length;j++){
+	                if(vetor[i].charAt(0)==vetorigual[j].charAt(0)){
+	                    contador++;
+	                }
+	            }
+	            vetorc[a]=contador;
+	            a+=1;
+	        }
+
+	        for(i=0;i<n;i++){
+	            System.out.print(vetor[i]+"="+vetorc[i]+" ");
+	        }
+	}
+	    public static void main(String[] args) {
+	        Scanner leitor = new Scanner(System.in);
+	        System.out.println("Diga quantos elementos tem seu vetor");
+	        int t = leitor.nextInt();
+	        String vetor[]= new String [t];
+	        for(int i=0;i<t;i++) {
+	            System.out.println("Escreva o elemento: " + i);
+	            vetor[i]= leitor.next();
+	        }
+	        Contagemdecaracteres(vetor);
+	        leitor.close();
+	    }
 	}
 
-}
+
